4d45759d8d18b9543e12611c6f97c9ae3e6c479a
Matheus Ribeiro <matheusriberomateus@gmail.com>
2024-08-20 11:26:48 -0300

    ajustes de cor e textos

--- pages/residencial.js
+++ pages/residencial.js
@@ -163,8 +163,8 @@ export default function Residencial() {
 
       <section className={styles.plansSection} id="plans">
         <div className={styles.container}>
-          <h2>Escolha o Plano Perfeito para Sua Empresa</h2>
-          <h4>Nossos planos foram desenvolvidos para atender a diferentes necessidades Empresariais.</h4>
+          <h2>Escolha o Plano Perfeito para Sua Casa</h2>
+          <h4>Nossos planos foram desenvolvidos para atender a diferentes necessidades residenciais.</h4>
           <div ref={sliderRef} className="keen-slider">
             <div className="keen-slider__slide">
               <Image src="/images/residencial/Para-Todos.webp" alt="Plano Para-Todos" width={250} height={450} />
